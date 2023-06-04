5d2566e3b9203350591af58d97b8f5f5f47ba277
TrippleA-Ashaba <trippleaunit@hotmail.com.com>
2023-06-04 18:25:59 +0300

    Run spiders from root dir

---
+++ run.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+
+# Change to the Scrapy project directory
+scrapy_project_path = BASE_PATH / "jobcrawler"
+os.chdir(scrapy_project_path)
+
+# Run the Scrapy spider
+spider_name = "quotes"
+subprocess.run(["scrapy", "crawl", spider_name])
